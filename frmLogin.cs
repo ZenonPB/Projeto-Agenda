f180980b24b12895c39babe0c7857febc421f7b0
Allicia Morete <allicia.morete4300@gmail.com>
2024-10-08 10:31:54 -0300

    ADD: telas, habilitar botao etc

---
+++ frmLogin.cs
@@ -0,0 +1,40 @@
+namespace ProjetoAgenda
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void habilitarBotaoLogin()
+        {
+            // checa se o tamanho do usuario e senha é maior que o estipulado e habilita o botão caso verdadeiro
+            if (tbxUsuario.TextLength > 0 && tbxSenha.TextLength >= 8)
+            {
+                btnLogin.Enabled = true;
+            }
+            else
+            {
+                btnLogin.Enabled = false;
+            }
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            frmCadastro frmCadastro = new frmCadastro();
+            frmCadastro.ShowDialog();
+
+        }
+
+        private void tbxUsuario_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
+
+        private void tbxSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
+    }
+}
